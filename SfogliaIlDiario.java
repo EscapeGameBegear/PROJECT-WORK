34af85ba9ec9a15079c4c6a5a281bc62ca9b9e1b
Elisa <61880548+Elisa98-hash@users.noreply.github.com>
2020-05-22 12:43:29 +0000

    Add files via upload

---
+++ SfogliaIlDiario.java
@@ -0,0 +1,43 @@
+package Diario;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class SfogliaIlDiario {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SfogliaIlDiario window = new SfogliaIlDiario();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SfogliaIlDiario() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
